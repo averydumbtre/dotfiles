a39c21eda89d7dce22589485362c2b3b0975742b
averydumbtre <mityasjunk@gmail.com>
2025-10-26 12:25:01 -0700

    automated commit

--- Documents/skripts/executable_SHUTDOWN.sh
+++ Documents/skripts/executable_SHUTDOWN.sh
@@ -1,6 +1,9 @@
-chezmoi re-add &> ~/.local/share/chezmoi/push.log
-chezmoi update &> ~/.local/share/chezmoi/push.log
-cd ~/.local/share/chezmoi/ &> ~/.local/share/chezmoi/push.log
-git add . &> ~/.local/share/chezmoi/push.log
-git commit -m "automated commit" &> ~/.local/share/chezmoi/push.log
-git push -u origin main &> ~/.local/share/chezmoi/push.log
+#! /bin/bash
+echo "finishing"
+chezmoi re-add &> /home/avery/.local/share/chezmoi/push.log
+chezmoi update &> /home/avery/.local/share/chezmoi/push.log
+cd /home/avery/.local/share/chezmoi/ &> /home/avery/.local/share/chezmoi/push.log
+git add . &> /home/avery/.local/share/chezmoi/push.log
+git commit -m "automated commit" &> /home/avery/.local/share/chezmoi/push.log
+git push -u origin main &> /home/avery/.local/share/chezmoi/push.log
+echo "chezmoi"
