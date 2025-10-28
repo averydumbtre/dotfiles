c5994db739727f98be5f265eb641653ab1a7b604
averydumbtre <mityasjunk@gmail.com>
2025-10-27 21:50:57 -0700

    automated commit

---
+++ Documents/skripts/kde_gamemode.sh
@@ -0,0 +1,4 @@
+cp /home/avery/.config/plasma-io.gitgud.wackyideas.desktop-appletsrc /home/avery/Documents/skripts/kde_widgets/plasma-io.gitgud.wackyideas.desktop-appletsrc
+rm /home/avery/.config/plasma-io.gitgud.wackyideas.desktop-appletsrc
+cp /home/avery/Documents/skripts/kde_widgets/MINIMAL/plasma-io.gitgud.wackyideas.desktop-appletsrc /home/avery/.config/plasma-io.gitgud.wackyideas.desktop-appletsrc
+systemctl --user restart plasma-plasmashell
