cad675389c87ea0d77098ee5c7d5419d39b7481e
averydumbtre <mityasjunk@gmail.com>
2025-10-27 23:08:07 -0700

    automated commit

--- Documents/skripts/kde_ungamemode.sh
+++ Documents/skripts/kde_ungamemode.sh
@@ -1,3 +1,4 @@
+#! /bin/bash
 cp /home/avery/.config/plasma-io.gitgud.wackyideas.desktop-appletsrc /home/avery/Documents/skripts/kde_widgets/plasma-io.gitgud.wackyideas.desktop-appletsrc
 rm /home/avery/.config/plasma-io.gitgud.wackyideas.desktop-appletsrc
 cp /home/avery/Documents/skripts/kde_widgets/FULL/plasma-io.gitgud.wackyideas.desktop-appletsrc /home/avery/.config/plasma-io.gitgud.wackyideas.desktop-appletsrc
