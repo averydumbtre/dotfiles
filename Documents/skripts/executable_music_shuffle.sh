da27eab101bff2d3e047756ebce71adfd7dd79aa
averydumbtre <mityasjunk@gmail.com>
2025-11-02 10:50:12 -0800

    automated commit

--- Documents/skripts/executable_music_shuffle.sh
+++ Documents/skripts/executable_music_shuffle.sh
@@ -2,4 +2,4 @@
 pipewire
 sleep 1
 pactl set-sink-volume @DEFAULT_SINK@ 100%
-mpv /run/media/avery/Game_Disk/music-flac/ -shuffle --no-video --audio-device=pipewire/alsa_output.pci-0000_00_1f.3.analog-stereo
+mpv /mnt/0df76e2c-45d5-44d2-ad7e-91488ebcafba/music-flac/ -shuffle --no-video --audio-device=pipewire/alsa_output.pci-0000_00_1f.3.analog-stereo
