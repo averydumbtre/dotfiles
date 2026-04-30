3ea6818b7363203165f31762cd6e5b93189cce51
averydumbtre <mityasjunk@gmail.com>
2026-04-30 01:05:22 -0700

    automated commit

---
+++ Documents/skripts/executable_DOTFILES.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+echo "finishing"
+chezmoi add /home/avery/.config/Ryujinx/bis/user/ &> /home/avery/.local/share/chezmoi/push.log
+chezmoi re-add &> /home/avery/.local/share/chezmoi/push.log
+##chezmoi update &> /home/avery/.local/share/chezmoi/push.log
+cd /home/avery/.local/share/chezmoi/ &> /home/avery/.local/share/chezmoi/push.log
+git add . &> /home/avery/.local/share/chezmoi/push.log
+git commit -m "automated commit" &> /home/avery/.local/share/chezmoi/push.log
+git push -u origin main &> /home/avery/.local/share/chezmoi/push.log
+echo "chezmoi"
