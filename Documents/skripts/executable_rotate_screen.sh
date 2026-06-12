d7181a0be10a01606384219a47e5a1746d1bc185
averydumbtre <mityasjunk@gmail.com>
2026-06-12 12:14:46 -0700

    automated commit

---
+++ Documents/skripts/executable_rotate_screen.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+ROTATION=$(export LC_ALL=en_US.utf8 && kscreen-doctor -o | grep 'Rotation' | cut -c 20- | sed 's/\x1B[@A-Z\\\]^_]\|\x1B\[[0-9:;<=>?]*[-!"#$%&'"'"'()*+,.\/]*[][\\@A-Z^_`a-z{|}~]//g')
+if [ "$ROTATION                                                                                                " = 1 ]; then
+    kscreen-doctor output.1.rotation.right
+    echo 'screen rotated right!'
+    exit
+fi
+    kscreen-doctor output.1.rotation.normal
+    echo 'screen rotated normal!'
+    exit
